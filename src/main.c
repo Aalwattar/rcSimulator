3f0b5fe9fff32a3b20eb0ba0bbe159c489cf3317
Ahmed Al-Wattar <aalwatta@uoguelph.ca>
2013-06-26 09:25:07 -0400

    Added main Refer to the last commit for details

---
+++ src/main.c
@@ -0,0 +1,38 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 24, 2013
+ *      Author: aalwatta
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "rcsSimulator.h"
+int main()
+{
+	struct SimData simData={.typeData={1,2,3,2,1,3,1,1,3,5}};
+	struct SimResults simResults={0};
+
+	simData.dFGID=0;
+	simData.noGPP=0;
+	simData.noPRR=5;
+
+	InitSimulator();
+	RunSimulator(&simData,&simResults);
+	CleanSimulator();
+
+
+	fprintf(stdout, "Process complete in {%d} cycles \n", simResults.totalTime);
+	fprintf(stdout,
+			"Number of configuration= %u SW Busy [%u] HW Busy [%u]\n",
+			simResults.noOfConfiguration, simResults.noSWBusyCounter,
+			simResults.noHWBusyCounter);
+	fprintf(stdout,
+			"SW2HW MIG [%u]  HW2SW Mig [%u] #of Reuse [%u]  #SW tasks [%u]\n",
+			simResults.noSW2HWMigration, simResults.noHW2SWMigration, simResults.noOfReuse,
+			simResults.noOfSWTasks);
+
+	return EXIT_SUCCESS;
+}
+
+
