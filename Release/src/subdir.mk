e5d9c137d6439a6bc40097bbf524389f2ba73619
Ratchette <jenn.winer@gmail.com>
2013-08-13 12:17:29 -0400

    Fixed makefile's dependancies

--- Release/src/subdir.mk
+++ Release/src/subdir.mk
@@ -47,7 +47,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/export/home/grad/aalwatta/include -I../../include -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I/export/home/grad/aalwatta/include -I../../../include -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
